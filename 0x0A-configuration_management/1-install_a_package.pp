4808575f4208e36d77a42f25790bdb1fefbc6f3e
ShogoMark <trigmark87@gmail.com>
2023-03-28 11:57:25 +0000

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# Installs flask
+# Installs flask, version 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
