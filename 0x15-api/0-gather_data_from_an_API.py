8858f73de4c89ed0e14e7075651af33c5762a019
ShogoMark <trigmark87@gmail.com>
2023-04-26 14:34:44 +0000

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,19 +7,19 @@ import sys
 if __name__ == "__main__":
 
 # API request to get employee information
-    emp_res = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1]))
-    emp_data = emp_res.json()
+	emp_res = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1]))
+    	emp_data = emp_res.json()
 	
-    print("Employee {} is done with tasks".format(emp_data.get('name')), end="")
+    	print("Employee {} is done with tasks".format(emp_data.get('name')), end="")
 
 # API request to get employee to-do list
-    emp_todo = requests.get("https://jsonplaceholder.typicode.com/todos/{}".format(sys.argv[1]))
-    todo_data = emp_todo.json()
-    completed_task = []
-    	for task in todo_data:
-            if task.get('completed') is True:
-                completed_task.append(task)
+    	emp_todo = requests.get("https://jsonplaceholder.typicode.com/todos/{}".format(sys.argv[1]))
+    	todo_data = emp_todo.json()
+    	completed_task = []
+    		for task in todo_data:
+            		if task.get('completed') is true:
+                		completed_task.append(task)
 
-	print("({}/{}):".format(len(completed_task), len(todo_data)))
-	for task in completed_task:
-		print("\t {}".format(task.get('title')))
+		print("({}/{}):".format(len(completed_task), len(todo_data)))
+			for task in completed_task:
+				print("\t {}".format(task.get('title')))
