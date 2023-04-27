7286f14de9a803cc54c90735c5a15a44147532ac
ShogoMark <trigmark87@gmail.com>
2023-04-27 18:50:53 +0000

    task2s

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
     j_task = emp_task.json()
     all_task = []
     for task in j_task:
-        all_task.append({"task": task["title"], "completed": task["completed"], "username": j_res.get('username')})
+        all_task.append({"task": task["title"], 
+                         "completed": task["completed"], "username": j_res.get('username')})
     filename = "{}.json".format(sys.argv[1])
     with open(filename, 'w') as json_file:
         json.dump({sys.argv[1]: all_task}, json_file)
-        
     with open(filename, 'r') as f:
         read_data = json.load(f)
         reverse_data = read_data[sys.argv[1]][::-1]
