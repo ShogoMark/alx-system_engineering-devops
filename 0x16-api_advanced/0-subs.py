3db3a626d81520c8780848b74d1180b197d2f7a9
ShogoMark <trigmark87@gmail.com>
2023-05-09 20:54:26 +0000

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A function that queries Reddit API returns no. of Subs"""
+
+import requests
+import sys
+
+def number_of_subscribers(subreddit):
+    """function takes in one parameter"""
+    subreddit = sys.argv[1];
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()['data']['subscribers']
+        return data
+    else:
+        return 0
