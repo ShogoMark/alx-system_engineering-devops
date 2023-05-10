4e110c97fc2ca734c6951222bc6de6f807fbaa62
ShogoMark <trigmark87@gmail.com>
2023-05-10 22:00:45 +0000

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,12 @@ import requests
 def top_ten(subreddit):
     """Function takes in one parameter"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent":"MyApp/1.0"}
+    headers = {"User-Agent": "MyApp/1.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get('data').get('children')
         for entry in data:
-               print(entry.get('data').get('title'))
+            print(entry.get('data').get('title'))
     else:
         print(None)
