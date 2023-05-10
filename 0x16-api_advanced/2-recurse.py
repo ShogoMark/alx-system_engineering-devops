999c39c7853c9d90535d330db1c9a4150c336659
ShogoMark <trigmark87@gmail.com>
2023-05-10 11:06:47 +0000

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A function a recursive function that queries 
+A recursive function that queries 
 the Reddit API and returns a list containing 
-the titles of all hot articles for a given subreddit
+the titles of all hot articles for a given subreddit.
 """
 
 import json
