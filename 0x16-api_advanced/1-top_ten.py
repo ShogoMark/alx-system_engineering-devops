08f242128b2415656ee4465ee188affa17791eb1
Shogo Akadiri <111003890+ShogoMark@users.noreply.github.com>
2023-05-10 22:34:21 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """A function that queries the Reddit API and prints query"""
 
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
     """Function takes in one parameter"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Custom User Agent"}
+    headers = {"User-Agent":"MyApp/1.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()['data']['children']
-        for post in data:
-            print(post['data']['title'])
+        data = response.json().get('data').get('children')
+        for entry in data:
+               print(entry.get('data').get('title'))
     else:
         print(None)
