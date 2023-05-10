949477bad0293581d59d4e0e514e4ffbb42d6e36
ShogoMark <trigmark87@gmail.com>
2023-05-10 11:01:47 +0000

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,14 +11,15 @@ import sys
 
 
 def recurse(subreddit, hot_list=[], after=None):
-"""A function takes in one parameter"""
+    """A function takes in one parameter"""
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     if after:
-        url += f"&after={after}"
+        url += f"?after={after}"
+   
     headers = {"User-Agent": "Custom User Agent"}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
        data  = response.json()['data']
        articles = data['children']
@@ -26,9 +27,10 @@ def recurse(subreddit, hot_list=[], after=None):
             list_title = article['data']['title']
             hot_list.append(list_title)
 
-       if data['after'] is not None:
+        if data['after'] is not None:
             return recurse(subreddit, hot_list, after=data['after'])
+        else:
+            return hot_list
+
     else:
         return None
-
-    return hot_list
