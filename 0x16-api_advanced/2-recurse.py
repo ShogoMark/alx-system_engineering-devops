9c82227b8554c0ac3586335b5454d45136c8c2fd
ShogoMark <trigmark87@gmail.com>
2023-05-12 11:33:52 +0000

    mod

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts for a given subreddit"""
+"""
+Recurse() - Function to query a list of
+all hot posts for a given subreddit
+"""
 
 import requests
 
